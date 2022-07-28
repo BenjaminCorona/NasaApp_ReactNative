a361d900137077e2a4a662335c8f5fd129bd3bd7
Benjamín <benja13lara@gmail.com>
2022-07-28 16:47:12 -0500

    se creo la api rest

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Image, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button } from "@rneui/base";
 import { Tab, Text, TabView } from "@rneui/themed";
 
-export default function App() {
-  const encenderApagar = () => {
-    console.log("Se muestra");
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const [nasa, setNasa] = useState([]);
+
+  const loadData = async () => {
+    const res = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=Z41yZjEh7Mh3H7tI8jrdtXMx8tbiZrOrsYaupuAO"
+    );
+    const data = await res.json();
+    setNasa(data);
   };
-  /**
-  const url =
-    "https://api.nasa.gov/planetary/apod?api_key=Z41yZjEh7Mh3H7tI8jrdtXMx8tbiZrOrsYaupuAO";
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => saveResult(json.hdurl));
 
-  function saveResult(json) {
-    console.log(json);
-  }
-
-  //const HDImg = `uri: "${json}"`;
-  const [selectedImage, setSelectedImage] = useState(null);
-   */
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <>
@@ -41,32 +39,58 @@ export default function App() {
         <Tab.Item
           title="Earth"
           titleStyle={{ fontSize: 12 }}
-          icon={{ name: "heart", type: "ionicon", color: "white" }}
+          icon={{ name: "earth", type: "ionicon", color: "white" }} //heart
         />
         <Tab.Item
-          title="Dev"
+          title="Exoplanets"
           titleStyle={{ fontSize: 12 }}
-          icon={{ name: "cart", type: "ionicon", color: "white" }}
+          icon={{ name: "planet", type: "ionicon", color: "white" }} //rocket
         />
       </Tab>
       {/*----------------------------------------------------------------------------------------*/}
       <TabView value={index} onChange={setIndex} animationType="spring">
         <TabView.Item style={styles.TabView}>
           <View style={styles.container}>
-            {/* <Text h4>Imágen Diaria</Text>*/}
+            <Text h4>{nasa.title}</Text>
             <Image
               source={{
-                uri: "https://pixabay.com/es/photos/beb%c3%a9-linda-retrato-ni%c3%b1o-indio-7318667/",
+                uri: nasa.hdurl,
               }}
               style={styles.Image}
             />
+            <Text h6>
+              <Text style={styles.negrita}>Copyright:</Text> {nasa.copyright}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>Date:</Text> {nasa.date}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>Explanation:</Text>
+              {nasa.explanation}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>HD:</Text> {nasa.hdurl}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>Media type:</Text> {nasa.media_type}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>Service version:</Text>
+              {nasa.service_version}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>Title:</Text> {nasa.title}
+            </Text>
+            <Text h6>
+              <Text style={styles.negrita}>Url:</Text> {nasa.url}
+            </Text>
           </View>
         </TabView.Item>
         <TabView.Item style={styles.TabView}>
-          <Text h1></Text>
+          <Text h1>Earth</Text>
         </TabView.Item>
         <TabView.Item style={styles.TabView}>
-          <Text h1></Text>
+          <Text h1>Exoplanets</Text>
         </TabView.Item>
       </TabView>
       {/*----------------------------------------------------------------------------------------*/}
@@ -90,30 +114,14 @@ const styles = StyleSheet.create({
     height: 3,
   },
   Image: {
-    width: "90%",
-    height: "90%",
-    borderRadius: 15,
-    //?La imagen contenida en el "image" se pondrá del mismo tamaño que el contenedor
-    resizeMode: "contain",
+    width: 350,
+    height: 350,
+    borderRadius: 9,
+    //resizeMode: "contain",
   },
+  negrita: { fontWeight: "bold" },
+  cursiva: { fontStyle: "italic" },
+  subrayado: { textDecorationLine: "underline" },
 });
 
-/**
- export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
- */
+export default App;
